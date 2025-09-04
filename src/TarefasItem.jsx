9ef9d3ed77b908027be569dd6bffbf02b9cd5ab1
Kauabasso <kauabasso8@gmail.com>
2025-09-04 11:30:33 -0300

    04/09

--- src/TarefasItem.jsx
+++ src/TarefasItem.jsx
@@ -1,6 +1,32 @@
-const TarefasItem = ({ tarefa }) => {
+import { useState } from "react";
+
+const TarefasItem = ({ tarefa, onEditTarefa, onDeleteTarefa  }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [novoTexto, setNovoTexto] = useState(tarefa.texto);
+
+    const handleEdit = () => {
+        if (isEditing) {
+            if (novoTexto.trim()) {
+                // Aqui grava o texto
+                onEditTarefa(tarefa.id, novoTexto);
+            }
+            setIsEditing(false);
+        } else{
+            setIsEditing(true);
+        }
+    }
+
+    const handleDelete = () => {
+        // Aqui deleta a tarefa
+        onDeleteTarefa(tarefa.id);
+    }
     return (
-        <li>{tarefa.texto}</li>
+        <li>
+            {isEditing ? (
+            <input type="text" value={novoTexto} onChange={(e) => setNovoTexto(e.target.value)} onKeyDown={(e)=>{if (e.key==='Enter') handleEdit()}} />) : (tarefa.texto)}
+            <button onClick={handleEdit}>Editar</button>
+            <button onClick={handleDelete}>X</button>
+        </li>
     );
 }
 
